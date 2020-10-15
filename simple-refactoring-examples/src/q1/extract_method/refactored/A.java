aa3e7db92742e7bb002bb4f5704e4e7acd776b1f
jonhermsen-UNO <jonhermsen@unomaha.edu>
2020-10-14 22:28:48 -0500

    refactor: use extract method refactoring by type parameterizing [q1]

--- simple-refactoring-examples/src/q1/extract_method/refactored/A.java
+++ simple-refactoring-examples/src/q1/extract_method/refactored/A.java
@@ -4,24 +4,28 @@ import java.util.List;
 
 public class A {
    Node m1(List<Node> nodes, String p) {
-      // TODO: Your answer
+      extractedMethod(nodes, p);
       // other implementation
       return null;
    }
 
    Edge m2(List<Edge> edgeList, String p) {
-      // TODO: Your answer
+      extractedMethod(edgeList, p);
       // other implementation
       return null;
    }
 
-   // TODO: Your answer
-   // ? extractedMethod(?) {
-   //
-   // }
+   /** Extract Method Refactoring by Type Parameterizing */
+   <T extends Graph> void extractedMethod(List<T> objs, String p) {
+      for (T obj : objs) {
+         if (obj.contains(p))
+            System.out.println(obj);
+      }
+   }
 }
 
-class Node {
+/** Extract Superclass Refactoring */
+class Graph {
    String name;
 
    public boolean contains(String p) {
@@ -29,10 +33,8 @@ class Node {
    }
 }
 
-class Edge {
-   String name;
+class Node extends Graph {
+}
 
-   public boolean contains(String p) {
-       return name.contains(p);
-   }
+class Edge extends Graph {
 }
